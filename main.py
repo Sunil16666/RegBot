d7798fd71885d1bd4b0abc86f2c0c0505b45b871
Sunil16666 <79449240+Sunil16666@users.noreply.github.com>
2022-08-25 15:16:53 +0200

    make test code work

--- main.py
+++ main.py
@@ -1,29 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
 
+browser = webdriver.Chrome()
 
-def test_eight_components():
-    driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()))
+browser.get('https://google.com')
+assert 'Google' in browser.title
 
-    driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+elem = browser.find_element(By.NAME, 'p')  # Find the search box
+elem.send_keys('seleniumhq' + Keys.RETURN)
 
-    title = driver.title
-    assert title == "Web form"
-
-    text_box = driver.find_element(by=By.NAME, value="my-text")
-    submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
-
-    text_box.send_keys("Selenium")
-    submit_button.click()
-
-    message = driver.find_element(by=By.ID, value="message")
-    value = message.text
-    assert value == "Received!"
-
-    driver.implicitly_wait(5)
-    driver.quit()
-
-
-test_eight_components()
+browser.quit()
